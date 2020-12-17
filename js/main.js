d375754ce7a56a36f54b598986ec4d247466c400
Ivan <gibson.ivan@gmail.com>
2020-12-17 12:20:33 -0500

    Nav and footer changes

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ carousel = document.querySelector("#truckCarousel");
 
 // stagger carousel not working, dont know why
 setTimeout(function() {
-    carousel.dataset.interval = 6000;
+    if(carousel != null){
+        carousel.dataset.interval = 6000;
+    }
 }, 3000);
